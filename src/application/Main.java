7c887810f981a16111fa5a2b0c2693374f78f377
iirlas <iirlas@yahoo.com>
2021-05-12 01:34:21 -0500

    Reformatted

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,10 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 
 public class Main extends Application {
 	@Override
@@ -15,32 +14,16 @@ public class Main extends Application {
 			Parent root = loader.load();
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-//
-//				@Override
-//				public void handle(KeyEvent event) {
-//					controller.handle(primaryStage, event);
-//				}
-//			});
-//			scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//
-//				@Override
-//				public void handle(MouseEvent event) {
-//					// TODO Auto-generated method stub
-//					controller.handle(primaryStage, event);
-//				}
-//			});
-			
 			primaryStage.setTitle("Atomic Penguin Warfare");
 			primaryStage.setScene(scene);
 			primaryStage.setFullScreen(false);
 			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
